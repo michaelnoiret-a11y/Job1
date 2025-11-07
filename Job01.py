698af498d85f06edb438934094de90588d5a9bf4
Michaël Noiret <michael.noiret@laplateforme.io>
2025-11-07 13:44:19 +0100

    Ajout code Jour6 Job01

---
+++ Job01.py
@@ -0,0 +1,10 @@
+"""" Écrire une fonction qui retourne une liste nommée « fruits » qui contient les 
+string suivants : «pomme », « cerise », « orange ». 
+Afficher en console le résultat de la fonction. """
+fruits = ["pomme", "cerise", "orange"] 
+def returnlist (fruits) :
+    #return fruits
+    print(fruits)
+#print(returnlist(fruits))
+returnlist(fruits)
+
